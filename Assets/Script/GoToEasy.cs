e6544e110bcdadc9af197fc1314b8ff60f2bf2d9
KillBarney <killbarney123@naver.com>
2023-10-09 14:36:00 +0900

    KoreanTruck

---
+++ Assets/Script/GoToEasy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToEasy : MonoBehaviour
+{
+    public void SceneChanges()
+    {
+        SceneManager.LoadScene("Easy");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
